7128767d156009cb5dba2f66d45bfe9ca9d4ec01
omar-su <73144494+Omar-su@users.noreply.github.com>
2022-04-19 18:05:38 +0200

    Created a service class which will handle the api requests

--- app/src/main/java/com/example/eurythmics/Credentials.java
+++ app/src/main/java/com/example/eurythmics/Credentials.java
@@ -2,8 +2,8 @@ package com.example.eurythmics;
 
 public class Credentials {
 
-    public static final String BASE_URL ="";
-    public static final String API_KEY = "";
+    public static final String BASE_URL ="https://api.themoviedb.org/";
+    public static final String API_KEY = "0279281a6298491c6675460bbefb7ccf";
 
 
 
