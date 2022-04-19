4bf6e98ad40346a83e6af480fc934da012251e59
omar-su <73144494+Omar-su@users.noreply.github.com>
2022-04-20 01:00:20 +0200

    Some implementation of MovieApi

--- app/src/main/java/com/example/eurythmics/MovieApi.java
+++ app/src/main/java/com/example/eurythmics/MovieApi.java
@@ -1,4 +1,20 @@
 package com.example.eurythmics;
 
+import com.example.eurythmics.response.MovieSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface MovieApi {
+
+    // https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
+    @GET
+    Call<MovieSearchResponse> searchMovie(
+            @Query("api_key") String key,
+            @Query("query") String query,
+            @Query("page") String page
+    );
+
+
 }
