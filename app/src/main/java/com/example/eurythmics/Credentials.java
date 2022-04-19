ee90c04d96abf90d0aab4429664dddfc23ed81b7
omar-su <73144494+Omar-su@users.noreply.github.com>
2022-04-19 16:34:36 +0200

    New branch and a new class for api requests

---
+++ app/src/main/java/com/example/eurythmics/Credentials.java
@@ -0,0 +1,21 @@
+package com.example.eurythmics;
+
+public class Credentials {
+
+    public static final String BASE_URL ="";
+    public static final String API_KEY = "";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
