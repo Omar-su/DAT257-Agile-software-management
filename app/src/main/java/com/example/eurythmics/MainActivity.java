fd980769af64036a87dbc94e221908879e993adf
cajenny <cajenny@student.chalmers.se>
2022-04-20 16:55:50 +0200

    Added functionality to change between views

--- app/src/main/java/com/example/eurythmics/MainActivity.java
+++ app/src/main/java/com/example/eurythmics/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.eurythmics;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_bar_nav);
+        final NavController navController = Navigation.findNavController(this, R.id.fragmentContainerView);
+
+       NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
