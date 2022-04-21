d09cb26a70f925e5fe0ddd8e92471791cb779aa8
staafd <staafd@student.chalmers.se>
2022-04-21 10:21:18 +0200

    Changes in main activity Java regarding navigation

--- app/src/main/java/com/example/eurythmics/MainActivity.java
+++ app/src/main/java/com/example/eurythmics/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.eurythmics;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -12,14 +13,38 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNav;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_bar_nav);
-        //final NavController navController = Navigation.findNavController(this, R.id.fragmentContainerView);
+        bottomNav = findViewById(R.id.bottom_bar_nav);
 
-       //NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        // init compoments
+        initBottomNavigationOnClick();
+
+
+
+    }
+    private  void  initBottomNavigationOnClick(){
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+            switch (item.getItemId()){
+                case R.id.homeFragment:
+                    fragment = new HomeFragment();
+                    break;
+
+                case R.id.ratingFragment:
+                    fragment = new ratingFragment();
+                    break;
+
+                case R.id.profileFragment:
+                    fragment = new profileFragment();
+                    break;
+            }
+            return true;
+        });
     }
 }
