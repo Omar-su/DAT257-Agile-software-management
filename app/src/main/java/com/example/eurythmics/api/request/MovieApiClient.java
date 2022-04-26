d546e139e6cb7faa511bdf7df4dcb8ead1319892
omar-su <73144494+Omar-su@users.noreply.github.com>
2022-04-26 17:34:16 +0200

    Executors and runnables implementation

--- app/src/main/java/com/example/eurythmics/api/request/MovieApiClient.java
+++ app/src/main/java/com/example/eurythmics/api/request/MovieApiClient.java
@@ -1,11 +1,24 @@
 package com.example.eurythmics.api.request;
 
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.eurythmics.AppExecutors;
+import com.example.eurythmics.api.Credentials;
 import com.example.eurythmics.api.models.MovieModel;
+import com.example.eurythmics.api.response.MovieSearchResponse;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class MovieApiClient {
 
@@ -13,6 +26,9 @@ public class MovieApiClient {
 
 
     private MutableLiveData<List<MovieModel>> mMovies;
+
+    //Making a global runnable request
+    private RetrieveMoviesRunnable retrieveMoviesRunnable;
 
     public static MovieApiClient getInstance() {
         if (instance == null){
@@ -27,6 +43,84 @@ public class MovieApiClient {
 
     public LiveData<List<MovieModel>> getMovies(){
         return mMovies;
+    }
+
+
+    public void searchMoviesApi(String query){
+        if (retrieveMoviesRunnable != null){
+            retrieveMoviesRunnable = null;
+        }
+
+        retrieveMoviesRunnable = new RetrieveMoviesRunnable(query);
+
+        final Future myHandler = AppExecutors.getInstance().networkIO().submit(retrieveMoviesRunnable);
+
+        AppExecutors.getInstance().networkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                // Canceling the retrofit call
+                myHandler.cancel(true);
+            }
+        },5000, TimeUnit.MICROSECONDS );
+
+    }
+
+    // retrieving data from rest api by runnable class
+    private class RetrieveMoviesRunnable implements Runnable{
+
+        private String query;
+        private boolean cancelRequest;
+
+        public RetrieveMoviesRunnable(String query) {
+            this.query = query;
+            this.cancelRequest = false;
+        }
+
+        @Override
+        public void run() {
+
+            // getting the response
+            try{
+                Response response = getMovies(query).execute();
+
+                if (cancelRequest){
+                    return;
+                }
+
+                if (response.code() == 200){
+                    List<MovieModel> movies = new ArrayList<>(((MovieSearchResponse)response.body()).getMovies());
+                    mMovies.postValue(movies);
+                }else {
+                    assert response.errorBody() != null;
+                    String error = response.errorBody().string();
+                    Log.d("TAG","Error" + error);
+                    mMovies.postValue(null);
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                mMovies.postValue(null);
+            }
+
+
+
+
+        }
+
+
+        // Search method/query
+        private Call<MovieSearchResponse> getMovies(String query){
+            return ServiceApi.getMovieApi().searchMovieByName(Credentials.API_KEY, query);
+        }
+
+        private void cancelRequest(){
+            Log.d("TAG", "Cancelling request");
+            cancelRequest = true;
+        }
+
+
+
     }
 
 
