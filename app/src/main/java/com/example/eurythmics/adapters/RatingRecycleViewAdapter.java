ef7dc79459ea3f27667a64e28f8cbd4791bdca08
omar-su <73144494+Omar-su@users.noreply.github.com>
2022-04-26 23:34:50 +0200

    Some layout and adapter implementation

---
+++ app/src/main/java/com/example/eurythmics/adapters/RatingRecycleViewAdapter.java
@@ -0,0 +1,29 @@
+package com.example.eurythmics.adapters;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RatingRecycleViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/eurythmics/adapters/RatingViewHolder.java
@@ -0,0 +1,38 @@
+package com.example.eurythmics.adapters;
+
+import static android.os.Build.VERSION_CODES.R;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.eurythmics.R;
+
+public class RatingViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+
+    TextView title, description;
+    ImageView poster;
+    RatingBar ratingBar;
+
+    // Click listener
+    OnMovieCardListener onMovieCardListener;
+
+
+    public RatingViewHolder(@NonNull View itemView, OnMovieCardListener onMovieCardListener) {
+        super(itemView);
+
+        title = itemView.findViewById(com.example.eurythmics.R.id.movie_title);
+
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        onMovieCardListener.onMovieClick(getAdapterPosition());
+    }
+}
