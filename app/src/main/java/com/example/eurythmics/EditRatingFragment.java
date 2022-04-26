3a8ee057085c67123ba0475b1e22fd6092a2eff0
cajenny <cajenny@student.chalmers.se>
2022-04-26 17:52:11 +0200

    Created controls for ratings

--- app/src/main/java/com/example/eurythmics/EditRatingFragment.java
+++ app/src/main/java/com/example/eurythmics/EditRatingFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,12 +13,10 @@ import androidx.fragment.app.Fragment;
 
 public class EditRatingFragment extends Fragment {
 
-
     @Nullable
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_edit_rating,container,false);
-
 
         return view;
     }
---
+++ app/src/main/java/com/example/eurythmics/Review/NumberPickerView.java
@@ -0,0 +1,40 @@
+package com.example.eurythmics.Review;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.example.eurythmics.R;
+
+public class NumberPickerView extends ConstraintLayout {
+
+    private ImageButton incrementButton;
+    private ImageButton decrementButton;
+    private EditText numberEditText;
+
+    public NumberPickerView(@NonNull Context context) {
+        super(context);
+    }
+
+    public NumberPickerView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NumberPickerView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        incrementButton = findViewById(R.id.increment);
+        decrementButton = findViewById(R.id.decrement);
+        numberEditText = findViewById(R.id.number);
+    }
+
+}
