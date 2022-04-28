0296a70ad2bca1b16306bf3ed0a54f777f3e1ae5
Fozboi <fabbejunior@hotmail.com>
2022-04-28 16:45:41 +0200

    Can almost save review to database

--- app/src/main/java/com/example/eurythmics/EditRatingFragment.java
+++ app/src/main/java/com/example/eurythmics/EditRatingFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
@@ -11,15 +13,40 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import database.DataBaseManager;
+
 public class EditRatingFragment extends Fragment {
+
+    DataBaseManager dbHelper;
+    private EditText overallRating;
+    private EditText notes;
+    private Button btnSave;
 
     @Nullable
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_edit_rating,container,false);
+        dbHelper = new DataBaseManager(null);
+        overallRating = view.findViewById(R.id.overallRatingLabel);
+        notes = view.findViewById(R.id.notesRatingLabel);
+        btnSave = view.findViewById(R.id.saveButton);
 
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newOverallRating = overallRating.getText().toString();
+                String newNotes = notes.getText().toString();
+                if (overallRating.length() != 0 && notes.length() != 0){
+                    AddRating(/** the movie title */, newOverallRating, newNotes);
+                }
+            }
+        });
         return view;
     }
+    //TODO: make this method get the title of the movie being rated
+    public void AddRating(String movieTitle, String overallRating, String notes) {
+        //boolean insertData = dbHelper.addReview(overallRating, notes);
 
+    }
 
 }
