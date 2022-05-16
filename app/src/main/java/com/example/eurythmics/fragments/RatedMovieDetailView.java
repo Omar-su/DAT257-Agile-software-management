d01c77568bb73e2a6d9505c785a35d18c46db0c0
omar-su <73144494+Omar-su@users.noreply.github.com>
2022-05-16 15:44:34 +0200

    Get rating for the detail view of the saved movies

--- app/src/main/java/com/example/eurythmics/fragments/RatedMovieDetailView.java
+++ app/src/main/java/com/example/eurythmics/fragments/RatedMovieDetailView.java
@@ -5,13 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
+import com.example.eurythmics.Movie.MovieService;
 import com.example.eurythmics.R;
+import com.example.eurythmics.Review.Review;
 import com.example.eurythmics.api.Credentials;
 import com.example.eurythmics.api.models.MovieModel;
 
@@ -20,13 +23,15 @@ import java.util.MissingResourceException;
 public class RatedMovieDetailView extends Fragment {
     private ImageView poster, likeButton;
 
-    private TextView title, category, releaseDate, durationTime;
+    private TextView title, category, releaseDate, durationTime,
+            storyRating, charactersRating, scoreRating, sceneryRating, overallRating;
 
     private Button addRatingButton;
 
     private MovieModel chosenMovie;
 
-    private boolean isFavorite = false;
+    private MovieService ms;
+
 
 
 
@@ -48,11 +53,19 @@ public class RatedMovieDetailView extends Fragment {
         title = view.findViewById(R.id.title_detail_view);
         releaseDate = view.findViewById(R.id.release_date_detail_view);
         poster = view.findViewById(R.id.poster_detail_view);
+        category = view.findViewById(R.id.category_detail_view);
+        storyRating = view.findViewById(R.id.story_value_movie_with_existing);
+        charactersRating = view.findViewById(R.id.chracters_value_movie_with_existing);
+        scoreRating= view.findViewById(R.id.score_value_movie_with_existing);
+        sceneryRating = view.findViewById(R.id.scenery_value_movie_with_existing);
+        overallRating = view.findViewById(R.id.overall_rating_value_movie_with_existing);
+
+        ms = MovieService.getMovieService();
 
 
         initLikeButton();
 
-        initAddRatingView();
+        initRatingView();
 
 
         getDataFromIntent();
@@ -65,10 +78,17 @@ public class RatedMovieDetailView extends Fragment {
         title.setText(chosenMovie.getTitle());
         releaseDate.setText(chosenMovie.getReleaseDate());
         Glide.with(this).load(Credentials.IMG_BASE_URL + chosenMovie.getPosterPath()).into(poster);
-
+        category.setText(chosenMovie.getCategory());
     }
 
-    private void initAddRatingView() {
+    private void initRatingView() {
+        Review review = ms.getReview(chosenMovie.getMovie_id());
+        storyRating.setText("" + review.getStoryRating());
+        charactersRating.setText("" + review.getCharactersRating());
+        scoreRating.setText("" + review.getScoreRating());
+        sceneryRating.setText("" + review.getSceneryRating());
+        overallRating.setText("" + review.getOverallRating());
+
     }
 
     private void initLikeButton() {
