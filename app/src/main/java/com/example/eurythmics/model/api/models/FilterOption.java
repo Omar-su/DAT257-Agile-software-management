a6ab60a6d79f5d84ebf0eb4dc46634017ec916dd
omar-su <73144494+Omar-su@users.noreply.github.com>
2022-05-17 12:29:37 +0200

    Added design filter and sort options

---
+++ app/src/main/java/com/example/eurythmics/model/api/models/FilterOption.java
@@ -0,0 +1,10 @@
+package com.example.eurythmics.model.api.models;
+
+/**
+ * Enumeration for the available filter options.
+ */
+public enum FilterOption {
+    INCOME,
+    EXPENSE,
+    ALL_CATEGORIES
+}
